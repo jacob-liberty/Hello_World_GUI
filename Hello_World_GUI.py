d29afad7a8129606082c0e4503bf47f5fcee6d93
Jacob Liberty <jacob.liberty@ocsbstudent.ca>
2017-09-13 15:48:41 -0400

    Initial Commit

---
+++ Hello_World_GUI.py
@@ -0,0 +1,9 @@
+# Created by: Jacob Liberty 
+# Created on: September 12 2017
+# Created for: ICS3U
+# This program is Hello, World! program, but with a GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
